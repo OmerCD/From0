ad0a4bfa4e3728ffce7cd22b2e12e85727da7797
OmerCD <omercd@hotmail.com.tr>
2016-09-08 12:44:44 +0300

    Oyuncu Yetenek Sistemi
    * Oyuncu Yetenek Sistemi koyuldu fakat test edilmedi ve gösterimi
    yapılmadı.

---
+++ Assets/Kodlar/Oyuncu Özellikleri/YetenekYönetim.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class YetenekYönetim : MonoBehaviour
+{
+    public string[] yetenekAdları;
+    class Yetenekler
+    {
+        const int YETENEK_ZORLUK_KATSAYISI = 2;
+        Yetenek[] sYetenekler;
+        public Yetenekler(string[] yetenekAdları)
+        {
+            sYetenekler = new Yetenek[yetenekAdları.Length];
+            for (int i = 0; i < yetenekAdları.Length; i++)
+            {
+                sYetenekler[i] = new Yetenek(yetenekAdları[i]);
+            }
+        }
+        public void YetenekArttır(string yetenekAdı,float değer)
+        {
+            this[yetenekAdı].SeviyeArttır(YETENEK_ZORLUK_KATSAYISI, değer);
+        }
+        public void YetenekArttır(int yetenekSıra, float değer)
+        {
+           this[yetenekSıra].SeviyeArttır(YETENEK_ZORLUK_KATSAYISI, değer);
+        }
+        public Yetenek this[string yetenekAdı]
+        {
+            get
+            {
+                foreach (Yetenek item in sYetenekler)
+                {
+                    if (item.YetenekAdı==yetenekAdı)
+                    {
+                        return item;
+                    }
+                }
+                return null;
+            }
+            set
+            {
+                for (int i = 0; i < sYetenekler.Length; i++)
+                {
+                    if (sYetenekler[i].YetenekAdı==yetenekAdı)
+                    {
+                        sYetenekler[i] = value;
+                    }
+                }
+            }
+        }
+        public Yetenek this[int sıra]
+        {
+            get
+            {
+                if (sıra>-1 && sıra<sYetenekler.Length)
+                {
+                    return sYetenekler[sıra];
+                }
+                return null;
+            }
+            set
+            {
+                if (sıra > -1 && sıra < sYetenekler.Length)
+                {
+                    sYetenekler[sıra]=value;
+                }
+            }
+        }
+    }
+    class Yetenek
+    {
+        string yetenekAdı;
+        float yetenekSeviyesi;
+        public void SeviyeArttır(int zorlukKatSayısı,float değer)
+        {
+            yetenekSeviyesi += değer / (zorlukKatSayısı * (int)yetenekSeviyesi);
+        }
+        public Yetenek(string yetenekAdı)
+        {
+            this.yetenekAdı = yetenekAdı;
+            yetenekSeviyesi = 0;
+        }
+
+        public Yetenek(string yetenekAdı, float yetenekSeviyesi)
+        {
+            this.yetenekAdı = yetenekAdı;
+            this.yetenekSeviyesi = yetenekSeviyesi;
+        }
+
+        public string YetenekAdı
+        {
+            get
+            {
+                return yetenekAdı;
+            }
+
+            set
+            {
+                yetenekAdı = value;
+            }
+        }
+
+        public float YetenekSeviyesi
+        {
+            get
+            {
+                return yetenekSeviyesi;
+            }
+
+            set
+            {
+                yetenekSeviyesi = value;
+            }
+        }
+    }
+    void Start()
+    {
+        Zaman.haftaDeğişti += HaftaDeğişimi;
+        Yetenekler oyuncuYetenekleri = new Yetenekler(yetenekAdları);
+    }
+    void HaftaDeğişimi() //Çalışılmayan yetenekler her hafta geçtiğinde burada körertilebilir
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            YetenekleriGöster();
+        }
+    }
+
+    private void YetenekleriGöster() // Yetenek Bilgi ekranı göstertilecek
+    {
+        throw new NotImplementedException();
+    }
+}
